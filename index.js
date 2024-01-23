8693b6d17deb778d48e8514476fdb31dbf66b8e2
anapaulas2x <annitaniunda@gmail.com>
2024-01-23 08:43:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+$('.navbar a').click(function (e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 30
+    }, 1000)
+});
+
+$('.navbar-mobile a').click(function (e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 30
+    }, 1000)
+});
+
+$(document).ready(function(){
+    var cargos = ["Desenvolvedora Front-End", "Analista de Suporte"];
+    var atual = 0;
+    $('.multiple-text').text(cargos[atual++]);
+    setInterval(function() {
+        $('.multiple-text').fadeOut(function() {
+            if (atual >= cargos.length) atual = 0;
+            $('.multiple-text').text(cargos[atual++]).fadeIn();
+        });
+    }, 2000);
+});
+
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+})
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('abrir-menu');
+})
